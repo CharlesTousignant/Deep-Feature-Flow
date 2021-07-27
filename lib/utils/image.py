8e4361bc5ee9be9f0caefe5a82d589e31bc3b83d
Charles Tousignant <charles.tousignant@polymtl.ca>
2021-07-27 17:04:29 -0400

    fix path error

--- lib/utils/image.py
+++ lib/utils/image.py
@@ -65,19 +65,20 @@ def get_pair_image(roidb, config):
         roi_rec = roidb[i]
 
         eq_flag = 0 # 0 for unequal, 1 for equal
+        print(roi_rec['image'])
         assert os.path.exists(roi_rec['image']), '%s does not exist'.format(roi_rec['image'])
         im = cv2.imread(roi_rec['image'], cv2.IMREAD_COLOR|cv2.IMREAD_IGNORE_ORIENTATION)
 
-        if roi_rec.has_key('pattern'):
-            ref_id = min(max(roi_rec['frame_seg_id'] + np.random.randint(config.TRAIN.MIN_OFFSET, config.TRAIN.MAX_OFFSET+1), 0),roi_rec['frame_seg_len']-1)
-            ref_image = roi_rec['pattern'] % ref_id
-            assert os.path.exists(ref_image), '%s does not exist'.format(ref_image)
-            ref_im = cv2.imread(ref_image, cv2.IMREAD_COLOR|cv2.IMREAD_IGNORE_ORIENTATION)
-            if ref_id == roi_rec['frame_seg_id']:
-                eq_flag = 1
-        else:
-            ref_im = im.copy()
-            eq_flag = 1
+        #if roi_rec.has_key('pattern'):
+        #    ref_id = min(max(roi_rec['frame_seg_id'] + np.random.randint(config.TRAIN.MIN_OFFSET, config.TRAIN.MAX_OFFSET+1), 0),roi_rec['frame_seg_len']-1)
+        #    ref_image = roi_rec['pattern'] % ref_id
+        #    assert os.path.exists(ref_image), '%s does not exist'.format(ref_image)
+        #    ref_im = cv2.imread(ref_image, cv2.IMREAD_COLOR|cv2.IMREAD_IGNORE_ORIENTATION)
+        #    if ref_id == roi_rec['frame_seg_id']:
+        #        eq_flag = 1
+
+        ref_im = im.copy()
+        eq_flag = 1
 
         if roidb[i]['flipped']:
             im = im[:, ::-1, :]
