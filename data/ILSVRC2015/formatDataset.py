fd23efa5ccb3bdd9989e41d9b034b15eed25abb2
Charles Tousignant <charles.tousignant@polymtl.ca>
2021-08-19 15:01:38 -0400

    number of classes fixed + dataset script changes for colab

--- data/ILSVRC2015/formatDataset.py
+++ data/ILSVRC2015/formatDataset.py
@@ -4,13 +4,12 @@ from os import makedirs, path
 import shutil
 import pathlib
 
-pathToDataset = "..\\code-master\\dataset\\twitch\\v1\\v1"
+pathToDataset = "../../../Twitch_dataset"
 indexNumIm = -5
 numNumbers = 4
-periodInFrames = 30 # 2fps
+periodInFrames = 30  # 2fps
 
 folders = os.listdir(pathToDataset)
-
 
 numVids = 0
 indexIm = 0
@@ -21,46 +20,45 @@ lastImageName = ""
 newImName = ""
 newAnnotationName = ""
 
-#make folder for txt files
+# make folder for txt files
 pathlib.Path("ImageSets").mkdir(parents=True, exist_ok=True)
-f = open("ImageSets\\Twitch_vid.txt", "a")
-t = open("ImageSets\\Twitch_vid_test.txt", "a")
-
+f = open("ImageSets/Twitch_vid.txt", "a")
+t = open("ImageSets/Twitch_vid_test.txt", "a")
 
 for folder in folders[:-1]:
     images = os.listdir(os.path.join(pathToDataset, folder, "image"))[:-1]
     for image in images:
-        
 
-        if(indexIm == 0) or (previousImageName != "" and (int(previousImageName[indexNumIm]) + 1) % 10 != int(image[indexNumIm])):
-
+        if (indexIm == 0) or (
+                previousImageName != "" and (int(previousImageName[indexNumIm]) + 1) % 10 != int(image[indexNumIm])):
             # make new folder for video data
-            currVidData = path.join("Data\\VID" , f"{numVids:04d}")
+            currVidData = path.join("Data/VID", "%04d" % (numVids,))  # f"{numVids:04d}"
             pathlib.Path(currVidData).mkdir(parents=True, exist_ok=True)
 
             # make new folder for annotations
-            currVidAnnotations = path.join("Annotations\\VID", f"{numVids:04d}")
-            pathlib.Path(currVidAnnotations).mkdir(parents=True, exist_ok=True)        
+            currVidAnnotations = path.join("Annotations/VID", "%04d" % (numVids,))
+            pathlib.Path(currVidAnnotations).mkdir(parents=True, exist_ok=True)
 
             numVids += 1
             indexIm = 0
 
         # copy and rename images and annotations
 
-        shutil.copyfile(path.join(pathToDataset, folder, "image", image), path.join(currVidData,image))          
-        newImName = f"{indexIm * periodInFrames:06d}" + ".JPEG"
+        shutil.copyfile(path.join(pathToDataset, folder, "image", image), path.join(currVidData, image))
+        newImName = "%06d" % (indexIm * periodInFrames,) + ".JPEG"
         os.rename(path.join(currVidData, image), path.join(currVidData, newImName))
 
         annotationName = image[:indexNumIm + 1] + ".xml"
-        shutil.copyfile(path.join(pathToDataset, folder, "image_annotation", annotationName), path.join(currVidAnnotations, annotationName))          
-        newAnnotationName = f"{indexIm * periodInFrames:06d}" + ".xml"
+        shutil.copyfile(path.join(pathToDataset, folder, "image_annotation", annotationName),
+                        path.join(currVidAnnotations, annotationName))
+        newAnnotationName = "%06d" % (indexIm * periodInFrames,) + ".xml"
         os.rename(path.join(currVidAnnotations, annotationName), path.join(currVidAnnotations, newAnnotationName))
 
-        if(numVids < 2000):
-            f.write(f"{numVids-1:04d}" + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
+        if (numVids < 2000):
+            f.write("%04d" % (numVids - 1,) + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
         else:
-            t.write(f"{numVids-1:04d}" + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
-            
+            t.write("%04d" % (numVids - 1,) + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
+
         indexIm += 1
-        
+
         previousImageName = image
