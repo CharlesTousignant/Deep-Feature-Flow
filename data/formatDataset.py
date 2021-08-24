b89678d5957d5d1ecafd4ca0d540d001b3b4cbbf
Charles Tousignant <charles.tousignant@polymtl.ca>
2021-08-24 17:38:54 -0400

    Fixed paths for test

--- data/formatDataset.py
+++ data/formatDataset.py
@@ -11,7 +11,7 @@ periodInFrames = 30  # 2fps
 
 folders = os.listdir(pathToDataset)
 
-numVids = 0
+numVids = -1
 indexIm = 0
 currVidData = ""
 currVidAnnotations = ""
@@ -26,20 +26,21 @@ f = open("ImageSets/Twitch_vid.txt", "a")
 t = open("ImageSets/Twitch_vid_test.txt", "a")
 
 for folder in folders[:-1]:
+
+    if (folder == "dset_Twitch_report.md"):
+        continue
+
     images = os.listdir(os.path.join(pathToDataset, folder, "image"))[:-1]
     for image in images:
 
         if (indexIm == 0) or (
                 previousImageName != "" and (int(previousImageName[indexNumIm]) + 1) % 10 != int(image[indexNumIm])):
+            numVids += 1
+
             # make new folder for video data
             currVidData = path.join("Data/VID", "%04d" % (numVids,))  # f"{numVids:04d}"
             pathlib.Path(currVidData).mkdir(parents=True, exist_ok=True)
 
-            # make new folder for annotations
-            currVidAnnotations = path.join("Annotations/VID", "%04d" % (numVids,))
-            pathlib.Path(currVidAnnotations).mkdir(parents=True, exist_ok=True)
-
-            numVids += 1
             indexIm = 0
 
         # copy and rename images and annotations
@@ -48,22 +49,29 @@ for folder in folders[:-1]:
         newImName = "%06d" % (indexIm * periodInFrames,) + ".JPEG"
         os.rename(path.join(currVidData, image), path.join(currVidData, newImName))
 
-        if (numVids < 1500):
-            f.write("%04d" % (numVids - 1,) + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
+        if (numVids < 300):
+            # make new folder for annotations
+            currVidAnnotations = path.join("Annotations/VID", "%04d" % (numVids,))
+            pathlib.Path(currVidAnnotations).mkdir(parents=True, exist_ok=True)
+
+            f.write("%04d" % (numVids,) + " 1 " + str(indexIm * periodInFrames) + " " + str(165) + '\n')
             annotationName = image[:indexNumIm + 1] + ".xml"
             shutil.copyfile(path.join(pathToDataset, folder, "image_annotation", annotationName),
                             path.join(currVidAnnotations, annotationName))
             newAnnotationName = "%06d" % (indexIm * periodInFrames,) + ".xml"
+
             os.rename(path.join(currVidAnnotations, annotationName), path.join(currVidAnnotations, newAnnotationName))
 
         else:
-            t.write("%04d" % (numVids - 1,) + " 1 " + '\n')
+            t.write("%04d" % (numVids,) + " 1 " + '\n')
+
             annotationName = image[:indexNumIm + 1] + ".xml"
             shutil.copyfile(path.join(pathToDataset, folder, "image_annotation", annotationName),
-                            path.join(currVidAnnotations, annotationName))
+                            path.join("Annotations/VID", annotationName))
             newAnnotationName = "%04d" % (numVids,) + ".xml"
-            pathsToAnnotation = currVidAnnotations.split()
-            os.rename(path.join(currVidAnnotations, annotationName), path.join(pathsToAnnotation[0], pathsToAnnotation[1], newAnnotationName))
+
+            os.rename(path.join("Annotations/VID", annotationName), path.join("Annotations/VID", newAnnotationName))
+            print(path.join("Annotations/VID", newAnnotationName))
 
         indexIm += 1
 
